a0662431c390fc7d7c80d1a8412dbbd827546a09
Nguyen Dinh Giang <dinhgiang2611@gmail.com>
2023-12-27 12:14:05 +0700

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var tradeRouter = require('./routes/trade');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/trade', tradeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
